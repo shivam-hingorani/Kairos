66ea7e8778fb567ad383470a43c9f3159d38a633
Shivam Hingorani <shivam_hingorani@brown.edu>
2025-02-01 15:06:56 -0500

    Initial Commit

---
+++ Kairos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kairos
+//
+//  Created by Shivam Hingorani on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kairos/KairosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairosApp.swift
+//  Kairos
+//
+//  Created by Shivam Hingorani on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct KairosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
